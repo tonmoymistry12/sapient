f91e31bc40f2be5aa2ba3e3a4cc9feae3792e1d2
tonmoy mistry <tonmoy@gamemarketinggenie.com>
2021-04-11 11:32:47 +0530

    added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FiltersComponent } from './views/filters/filters.component';
+import { MissionDetailsComponent } from './views/mission-details/mission-details.component';
+import { HomeComponent } from './views/home/home.component';
+import { LayoutComponent } from './views/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent,
+    MissionDetailsComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/mission-details/mission-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-details',
+  templateUrl: './mission-details.component.html',
+  styleUrls: ['./mission-details.component.scss']
+})
+export class MissionDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
